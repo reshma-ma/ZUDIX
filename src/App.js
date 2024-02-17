31c24e0a6dfaeafa18cf29088f4eb62800345c6f
Reshma.ma <reshma.ma1998@gmail.com>
2024-02-17 01:35:23 +0100

    shopping pages and footer added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import LoginSignup from './Pages/LoginSignup';
 import Shop from './Pages/Shop';
 import Product from './Pages/Product';
 import Cart from './Pages/Cart';
+import Footer from './Components/Footer/Footer'
 
 
 function App() {
@@ -25,6 +26,7 @@ function App() {
         <Route path='/login' element={<LoginSignup/>}/>
         
       </Routes>
+      <Footer/>
       </BrowserRouter>
     </div>
   );
---
+++ src/Context/ShopContext.js
@@ -0,0 +1,9 @@
+// import React,{createContext} from "react";
+
+
+
+// export const ShopContext= createContext(null);
+
+// const ShopContextProvider=(props)=>{
+//     const contextValue={}
+// }
