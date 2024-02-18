989b19571051504cefbd9f6568e27491073f7fec
Reshma.ma <reshma.ma1998@gmail.com>
2024-02-18 03:51:15 +0100

    login page added

--- src/Context/ShopContext.js
+++ src/Context/ShopContext.js
@@ -1,9 +1,17 @@
-// import React,{createContext} from "react";
+import React,{createContext} from "react";
+import all_product from '../Components/Assets/all_product'
 
 
 
-// export const ShopContext= createContext(null);
+export const ShopContext= createContext(null);
 
-// const ShopContextProvider=(props)=>{
-//     const contextValue={}
-// }
+const ShopContextProvider=(props)=>{
+    const contextValue={all_product};
+    return (
+        <ShopContext.Provider value={contextValue}>
+            {props.children}
+        </ShopContext.Provider>
+    )
+}
+
+export default ShopContextProvider;
